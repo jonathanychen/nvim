3895b77c9cdfacef30f5d24b6c18caad5616b4a6
Jonathan Chen <jonathan.chenjc15243@gmail.com>
2025-01-13 19:22:36 -0500

    completion

---
+++ lua/jonathanychen/keymap.lua
@@ -0,0 +1,4 @@
+vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
+vim.keymap.set('n', 'gra', vim.lsp.buf.code_action)
+vim.keymap.set('n', 'grr', vim.lsp.buf.references)
+
---
+++ lua/jonathanychen/lazy/lsp.lua
@@ -0,0 +1,37 @@
+return {
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "saghen/blink.cmp",
+            {
+                "folke/lazydev.nvim",
+                ft = "lua",
+                opts = {
+                    library = {
+                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                    },
+                },
+            },
+        },
+        config = function()
+            require("lspconfig").lua_ls.setup {}
+            require("lspconfig").pyright.setup {}
+
+            vim.api.nvim_create_autocmd('LspAttach', {
+                callback = function(args)
+                    local client = vim.lsp.get_client_by_id(args.data.client_id)
+                    if not client then return end
+
+                    if client.supports_method('textDocument/formatting') then
+                        vim.api.nvim_create_autocmd('BufWritePre', {
+                            buffer = args.buf,
+                            callback = function()
+                                vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
+                            end
+                        })
+                    end
+                end,
+            })
+        end,
+    },
+}
