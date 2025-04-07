e4bd739f3904e51c63d9d04fc3940a0d08f3db63
Jonathan Chen <jonathanyangchen@gmail.com>
2025-04-07 02:30:05 -0400

    always show diagnostics in-line

--- lua/jonathanychen/lazy/lsp.lua
+++ lua/jonathanychen/lazy/lsp.lua
@@ -37,6 +37,10 @@ return {
                 lspconfig[server].setup(config)
             end
 
+            vim.diagnostic.config({
+                virtual_text = true
+            })
+
             vim.api.nvim_create_autocmd('LspAttach', {
                 callback = function(args)
                     local client = vim.lsp.get_client_by_id(args.data.client_id)
