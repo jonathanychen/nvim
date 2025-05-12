2ffdada1d84fd249544ba307499973d15db268dd
Jonathan Chen <jonathanyangchen@gmail.com>
2025-05-12 17:19:13 -0400

    file browser

--- lua/jonathanychen/keymap.lua
+++ lua/jonathanychen/keymap.lua
@@ -2,3 +2,4 @@ vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
 vim.keymap.set('n', 'gra', vim.lsp.buf.code_action)
 vim.keymap.set('n', 'grr', vim.lsp.buf.references)
 
+vim.keymap.set("n", "<space>fb", ":Telescope file_browser<CR>")
