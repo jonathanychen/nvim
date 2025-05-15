092c80a6fb7abaee593a9fb7224f1a708b5862af
Jonathan Chen <jonathanyangchen@gmail.com>
2025-05-14 22:21:56 -0400

    telescope shortcuts

--- lua/jonathanychen/keymap.lua
+++ lua/jonathanychen/keymap.lua
@@ -2,4 +2,10 @@ vim.keymap.set('n', 'grn', vim.lsp.buf.rename)
 vim.keymap.set('n', 'gra', vim.lsp.buf.code_action)
 vim.keymap.set('n', 'grr', vim.lsp.buf.references)
 
-vim.keymap.set("n", "<space>fb", ":Telescope file_browser<CR>")
+
+local telescope_builtin = require('telescope.builtin')
+
+vim.keymap.set("n", "<space>fb", ":Telescope file_browser<CR>", { desc = 'Telescope browse files' })
+vim.keymap.set("n", "<space>ff", telescope_builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set("n", "<space>fg", telescope_builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set("n", "<space>fh", telescope_builtin.help_tags, { desc = 'Telescope help tags' })
