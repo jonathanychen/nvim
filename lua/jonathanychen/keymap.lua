bfd40abd727809c628b33c20cd0899cb7767fb59
Jonathan Chen <jonathan.chenjc15243@gmail.com>
2025-06-17 23:58:17 -0700

    relative movements for wrapped lines

--- lua/jonathanychen/keymap.lua
+++ lua/jonathanychen/keymap.lua
@@ -9,3 +9,7 @@ vim.keymap.set("n", "<space>fb", ":Telescope file_browser<CR>", { desc = 'Telesc
 vim.keymap.set("n", "<space>ff", telescope_builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set("n", "<space>fg", telescope_builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set("n", "<space>fh", telescope_builtin.help_tags, { desc = 'Telescope help tags' })
+
+-- Move within visual lines
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
